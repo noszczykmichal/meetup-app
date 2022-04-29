4afb9c2494bf7879b106cedf9eaf4bbcf373f19f
Michal <noszczykmichal@wp.pl>
2022-04-29 04:27:29 +0200

    updating State with use of React Context

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
+import {FavoritesContextProvider} from "./store/favorites-context";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>
 );
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,16 @@
+import {useContext} from 'react';
+import FavoritesContext from '../store/favorites-context'
+
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoritesCtx= useContext(FavoritesContext);
+
+  return <section>
+  <h1>My Favorites</h1>
+  {favoritesCtx.totalFavorites? <MeetupList data={favoritesCtx.favorites}/> : <p>You got no favorites.</p>}
+</section>
+  
 }
 
 export default FavoritesPage;
