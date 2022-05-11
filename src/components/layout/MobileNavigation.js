c6666b5074cda95a5ba91dd57ecf56f0ee258cc2
Michal <noszczykmichal@wp.pl>
2022-05-11 15:37:04 +0200

    improved styling of modal

--- src/components/layout/MobileNavigation.js
+++ src/components/layout/MobileNavigation.js
@@ -18,30 +18,18 @@ function MobileNavigation(props) {
   return (
     <nav className={attachedClasses.join(" ")}>
       <ul className={classes["mobile-navbar__items"]}>
-        <li>
-          <Link
-            to="/"
-            onClick={props.linkClicked}
-            className={classes["mobile-navbar__item"]}
-          >
+        <li className={classes["mobile-navbar__item"]}>
+          <Link to="/" onClick={props.linkClicked}>
             All Meetups
           </Link>
         </li>
-        <li>
-          <Link
-            to="/new-meetup"
-            onClick={props.linkClicked}
-            className={classes["mobile-navbar__item"]}
-          >
+        <li className={classes["mobile-navbar__item"]}>
+          <Link to="/new-meetup" onClick={props.linkClicked}>
             Add New Meetup
           </Link>
         </li>
-        <li>
-          <Link
-            to="/favorites"
-            onClick={props.linkClicked}
-            className={classes["mobile-navbar__item"]}
-          >
+        <li className={classes["mobile-navbar__item"]}>
+          <Link to="/favorites" onClick={props.linkClicked}>
             My Favorites
           </Link>
           <div className={classes["mobile-navbar__item--badge"]}>
